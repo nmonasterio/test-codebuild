061e9d2f32d54590917f6967710967c189955b9a
Napo Monasterio <nmonasterio@daxko.com>
2018-04-20 10:34:38 -0500

    Testing things.

---
+++ envs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -L -o aws-env https://github.com/Droplr/aws-env/raw/master/bin/aws-env-darwin-amd64
+chmod +x ./aws-env
+
+if [[ $CI_BRANCH =~ .*beta ]]
+then
+    echo "There's beta!"
+elif [[ $CI_BRANCH =~ .*staging ]]
+then
+    echo "There's staging!"
+elif [[ $CI_BRANCH =~ .*master ]]
+then
+    echo "There's master!"
+else
+    echo "None of the above!"
+fi

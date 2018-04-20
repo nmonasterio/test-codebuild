b017b25c338c1cc603ced2e1cf46055db0b04127
Napo Monasterio <nmonasterio@daxko.com>
2018-04-20 10:41:15 -0500

    Tests.

--- envs.sh
+++ envs.sh
@@ -2,7 +2,9 @@
 
 curl -L -o aws-env https://github.com/Droplr/aws-env/raw/master/bin/aws-env-linux-amd64
 chmod +x ./aws-env
-
+echo "Branch is $CI_BRANCH"
+eval $(AWS_ENV_PATH=/dev/partnerships/ AWS_REGION=us-east-1 ./aws-env)
+echo "Test one is $DEV_ETL_SERVER"
 if [[ $CI_BRANCH =~ .*beta ]]
 then
     echo "There's beta!"

af4fbd4045fd671cb7da0bac5e049b2e7f2456a5
Napo Monasterio <nmonasterio@daxko.com>
2018-04-20 12:34:54 -0500

    -

--- envs.sh
+++ envs.sh
@@ -4,6 +4,7 @@ echo $"AWS_REGION"
 
 BRANCH=`git branch -a --contains $CODEBUILD_SOURCE_VERSION | sed -n 2p || $CODEBUILD_SOURCE_VERSION`
 export CI_BRANCH="$BRANCH"
+echo "$CI_BRANCH"
 curl -L -o aws-env https://github.com/Droplr/aws-env/raw/master/bin/aws-env-linux-amd64
 chmod +x ./aws-env
 

ba1839a2bc4647b9bbbadd2ef8e8b9fad8d8d8b3
Napo Monasterio <nmonasterio@daxko.com>
2018-04-20 12:19:30 -0500

    Testing.

--- envs.sh
+++ envs.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-#curl -L -o aws-env https://github.com/Droplr/aws-env/raw/master/bin/aws-env-linux-amd64
-#chmod +x ./aws-env
-# echo "Branch is $CI_BRANCH"
-# eval $(AWS_ENV_PATH=/dev/partnerships/ AWS_REGION=us-east-1 ./aws-env)
-# echo "Test one is $DEV_ETL_SERVER"
-if [[ $CI_BRANCH =~ .*beta ]] \
-then
-    echo "There's beta!"
-elif [[ $CI_BRANCH =~ .*staging ]]
-then
-    echo "There's staging!"
-elif [[ $CI_BRANCH =~ .*master ]]
-then
-    echo "There's master!"
-else
-    echo "None of the above!"
-fi
-$a
-$([[ "$CI_BRANCH" =~ .*beta ]] && echo "$a=dev" || echo "$a=foo")
-echo "A is... $a"
+export AWS_REGION=us-east-1
+BRANCH=`git branch -a --contains $CODEBUILD_SOURCE_VERSION | sed -n 2p || $CODEBUILD_SOURCE_VERSION`
+export CI_BRANCH="$BRANCH"
+curl -L -o aws-env https://github.com/Droplr/aws-env/raw/master/bin/aws-env-darwin-amd64
+chmod +x ./aws-env
+SECRETS_PREFIX=""
+[[ $CI_BRANCH =~ .*beta ]] && { SECRETS_PREFIX="dev"; true; } || true;
+[[ $CI_BRANCH =~ .*staging ]] && { SECRETS_PREFIX="staging"; true; } || true;
+[[ $CI_BRANCH =~ .*master ]] && { SECRETS_PREFIX="master"; true; } || true;
+export AWS_ENV_PATH="/$SECRETS_PREFIX/partnerships/"
+
+[ -n "$SECRETS_PREFIX" ] && { eval $(./aws-env); true; } || echo "Not building from a branch that requires envs"; true;
+env | grep DEV_
